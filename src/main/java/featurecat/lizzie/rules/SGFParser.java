066e7ad5e372e669bb3de4986896f2432882531c
zsalch <zsal@163.com>
2018-09-19 18:04:13 +0800

    Remove unnecessary code

--- src/main/java/featurecat/lizzie/rules/SGFParser.java
+++ src/main/java/featurecat/lizzie/rules/SGFParser.java
@@ -88,7 +88,7 @@ public class SGFParser {
 
         String blackPlayer = "", whitePlayer = "";
 
-        // Suppoert unicode charactors (UTF-8)
+        // Support unicode characters (UTF-8)
         for (int i = 0; i < value.length(); i++) {
             char c = value.charAt(i);
             if (escaping) {
@@ -115,7 +115,8 @@ public class SGFParser {
                     if (!inTag) {
                         if (isMultiGo) {
                             // Restore to the variation node
-                            for (int s = 0; s < subTreeStepMap.get(Integer.valueOf(subTreeDepth)).intValue(); s++) {
+                            int varStep = subTreeStepMap.get(subTreeDepth);
+                            for (int s = 0; s < varStep; s++) {
                                 Lizzie.board.previousMove();
                             }
                         }
@@ -154,7 +155,7 @@ public class SGFParser {
                             Lizzie.board.pass(Stone.BLACK);
                         } else {
                             // Save the step count
-                            subTreeStepMap.put(Integer.valueOf(subTreeDepth), Integer.valueOf(subTreeStepMap.get(Integer.valueOf(subTreeDepth)).intValue() + 1));
+                            subTreeStepMap.put(subTreeDepth, subTreeStepMap.get(subTreeDepth) + 1);
                             Lizzie.board.place(move[0], move[1], Stone.BLACK);
                         }
                     } else if (tag.equals("W")) {
@@ -163,7 +164,7 @@ public class SGFParser {
                             Lizzie.board.pass(Stone.WHITE);
                         } else {
                             // Save the step count
-                            subTreeStepMap.put(Integer.valueOf(subTreeDepth), Integer.valueOf(subTreeStepMap.get(Integer.valueOf(subTreeDepth)).intValue() + 1));
+                            subTreeStepMap.put(subTreeDepth, subTreeStepMap.get(subTreeDepth) + 1);
                             Lizzie.board.place(move[0], move[1], Stone.WHITE);
                         }
                     }  else if (tag.equals("C")) {
@@ -340,15 +341,17 @@ public class SGFParser {
 
 
         // Write variation tree
-        builder.append(generateNode(board, writer, history.nextNode()));
+        builder.append(generateNode(board, history.nextNode()));
 
         // close file
         builder.append(')');
         writer.append(builder.toString());
     }
 
-    // Generate node
-    private static String generateNode(Board board, Writer writer, BoardHistoryNode node) throws IOException {
+    /**
+     * Generate node with variations
+     */
+    private static String generateNode(Board board, BoardHistoryNode node) throws IOException {
         StringBuilder builder = new StringBuilder("");
 
         if (node != null) {
@@ -378,11 +381,11 @@ public class SGFParser {
                     // Variation
                     for (BoardHistoryNode sub : node.getNexts()) {
                         builder.append("(");
-                        builder.append(generateNode(board, writer, sub));
+                        builder.append(generateNode(board, sub));
                         builder.append(")");
                     }
                 } else if (node.numberOfChildren() == 1) {
-                    builder.append(generateNode(board, writer, node.next()));
+                    builder.append(generateNode(board, node.next()));
                 } else {
                     return builder.toString();
                 }
