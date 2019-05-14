0669cc119a9c4fbf4ab49247879081bc144b1ffd
zsalch <zsal@163.com>
2019-05-14 14:57:51 +0800

    Default to turn off the hold winrate in the move

--- src/main/java/featurecat/lizzie/rules/SGFParser.java
+++ src/main/java/featurecat/lizzie/rules/SGFParser.java
@@ -219,7 +219,7 @@ public class SGFParser {
             } else {
               Lizzie.board.comment(tagContent);
             }
-          } else if (tag.equals("LZ")) {
+          } else if (tag.equals("LZ") && Lizzie.config.holdWinrateToMove) {
             // Content contains data for Lizzie to read
             String[] lines = tagContent.split("\n");
             String[] line1 = lines[0].split(" ");
@@ -496,7 +496,9 @@ public class SGFParser {
         }
 
         // Add LZ specific data to restore on next load
-        builder.append(String.format("LZ[%s]", formatNodeData(node)));
+        if (Lizzie.config.holdWinrateToMove) {
+          builder.append(String.format("LZ[%s]", formatNodeData(node)));
+        }
       }
 
       if (node.numberOfChildren() > 1) {
