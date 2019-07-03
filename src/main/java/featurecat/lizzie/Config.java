227260a2e0aee2d299244f9a1c5ae6342ebd4078
zsalch <zsal@163.com>
2019-07-03 13:29:13 +0800

    Add control option for the best moves in the move

--- src/main/java/featurecat/lizzie/Config.java
+++ src/main/java/featurecat/lizzie/Config.java
@@ -98,6 +98,8 @@ public class Config {
   public Optional<Map<Double, Color>> blunderNodeColors;
   public int nodeColorMode = 0;
   public boolean appendWinrateToComment = true;
+  public boolean holdBestMovesToSgf = true;
+  public boolean showBestMovesByHold = true;
   public int boardPositionProportion = 4;
   public int limitBestMoveNum = 0;
   public int limitBranchLength = 0;
@@ -210,6 +212,8 @@ public class Config {
     handicapInsteadOfWinrate = uiConfig.getBoolean("handicap-instead-of-winrate");
     showDynamicKomi = uiConfig.getBoolean("show-dynamic-komi");
     appendWinrateToComment = uiConfig.optBoolean("append-winrate-to-comment");
+    holdBestMovesToSgf = uiConfig.optBoolean("hold-bestmoves-to-sgf", true);
+    showBestMovesByHold = uiConfig.optBoolean("show-bestmoves-by-hold", true);
     showCoordinates = uiConfig.optBoolean("show-coordinates");
     replayBranchIntervalSeconds = uiConfig.optDouble("replay-branch-interval-seconds", 1.0);
     colorByWinrateInsteadOfVisits = uiConfig.optBoolean("color-by-winrate-instead-of-visits");
